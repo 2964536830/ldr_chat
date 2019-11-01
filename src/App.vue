8c2d9d7342e5855796f1b6a30e058b90c9cba274
sakura_zhang <17310408750@163.com>
2019-11-01 16:43:01 +0800

    初始化,添加库

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld />
+    </v-content>
+  </v-app>
 </template>
+
 <script>
+import HelloWorld from './components/HelloWorld'
 import { APPKEY } from './core/var'
+
 export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data: () => ({
+    //
+  }),
   created () {
     let time = new Date().getTime()
     const signature = md5(`appkey=${APPKEY}&timestamp=${time}&random_str=022cd9fd995849b58b3ef0e943421ed9&key=5a4420a03fae3ed6f8f5b658`)
@@ -42,22 +61,3 @@ export default {
   }
 }
 </script>
-<style lang="less">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <Button>你好</Button>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '../components/HelloWorld'
+
 export default {
-  name: 'home',
   components: {
     HelloWorld
-  },
-  data () {
-    return {
-      text: ''
-    }
-  },
-  mounted () {
-    console.log(JIM)
-    this.reg()
-  },
-  methods: {
-    reg () {
-
-    }
   }
 }
 </script>
